b9b3fb8d6226a55de21298b629c0d8240132d888
Arush-Developer <arushg.official@gmail.com>
2025-10-18 18:38:57 +0530

    Add InnovationArcade route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import BenefitsPage from './pages/BenefitsPage';
 import ContactPage from './pages/ContactPage';
+import InnovationArcade from './pages/InnovationArcade';
 import UserDashboard from './components/UserDashboard';
 import Marketplace from './components/Marketplace';
 
@@ -73,6 +74,7 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
             <Route path="/benefits" element={<BenefitsPage />} />
             <Route path="/contact" element={<ContactPage />} />
+            <Route path="/arcade" element={<InnovationArcade />} />
             <Route path="/marketplace" element={<Marketplace />} />
             <Route
               path="/dashboard"
